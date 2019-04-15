6f65f0fef4bc7af1d9d70dc93215ad5429f2b07a
dexen deVries <dexen.devries@gmail.com>
2019-04-15 17:54:29 +0200

    generate nice file names

--- libs.php
+++ libs.php
@@ -6,3 +6,19 @@ function td(...$a)
 		var_dump($v);
 	die('td()');
 }
+
+function propose_ca_files() : array
+{
+	return [
+		'ca_key_file' => sprintf('%s-%s-CA.key',
+			get_current_user(),
+			gethostname() ),
+		'ca_cert_file' => sprintf('%s-%s-CA.pem',
+			get_current_user(),
+			gethostname() ),
+	];
+}
+
+function generate_ca_files(array $config)
+{
+}
